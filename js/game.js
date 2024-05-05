65665d01774a87faebc55fc50f39dfa726278d64
yonatanchuraro <168965215+yonatanchuraro@users.noreply.github.com>
2024-05-05 18:01:57 +0300

    init

---
+++ js/game.js
@@ -0,0 +1,163 @@
+'use strict'
+const MINE = '💣'
+const EMPTY = ''
+const FLAG = '🚩'
+const size = 4
+const mineCounter = 2
+var flagConter = 0
+var gBoard = buildBoard()
+var gameOver = false
+const elBtn = document.querySelector('.conteiner button')
+
+
+// onInit function
+function onInit() {
+    elBtn.innerText='😃'
+    gBoard = buildBoard()
+    renderBoard(gBoard)
+}
+
+
+// build board function
+function buildBoard() {
+    var board = []
+    // First, build the entire board
+    for (var i = 0; i < size; i++) {
+        board.push([])
+        for (var j = 0; j < size; j++) {
+            board[i][j] = {
+                minesAroundCount: 0,
+                isShown: false,
+                isMine: false,
+                isMarked: false
+            }
+        }
+    }
+    // Then, update the minesAroundCount for cells with mines
+    for (var i = 0; i < size; i++) {
+        for (var j = 0; j < size; j++) {
+            if (i === 1 && j === 2 || i === 2 && j === 1) {
+                board[i][j].isMine = true
+            } else {
+                board[i][j].minesAroundCount = setMinesNegsCount(board, i, j)
+            }
+        }
+    }
+    return board
+}
+
+
+// render board function
+function renderBoard(board) {
+    var strHTML = ''
+    for (var i = 0; i < board.length; i++) {
+        strHTML += '<tr>'
+
+        for (var j = 0; j < board[i].length; j++) {
+            const cell = board[i][j]
+            const className = `cell cell-${i}-${j}`
+            strHTML += `<td class="${className}" onclick="onCellClicked(this,${i},${j})"
+             oncontextmenu="onRightClick(this,${i},${j})">${EMPTY}</td>`
+        }
+
+
+        strHTML += '</tr>'
+    }
+    const elBoard = document.querySelector('.board')
+    elBoard.innerHTML = strHTML
+}
+
+
+// set mines negs count function
+function setMinesNegsCount(board, rowIdx, collIdx) {
+    var negsCounter = 0
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+        if (i < 0 || i > 3) continue
+
+        for (var j = collIdx - 1; j <= collIdx + 1; j++) {
+            if (j < 0 || j > 3) continue
+            if (i === rowIdx && j === collIdx) continue
+            if (board[i][j].isMine === true) negsCounter++
+        }
+    }
+    return negsCounter
+}
+
+
+// on cell click function
+function onCellClicked(elCell, i, j) {
+    checkGameOver(gBoard)
+    var sound = new Audio('js/click.mp3')
+    const cell = gBoard[i][j]
+    const negsCounter = setMinesNegsCount(gBoard, i, j)
+    if (gameOver) {
+        return
+    }
+    sound.play()
+    elCell.style.backgroundColor = 'grey'
+    if (!cell.isShown) {
+        cell.isShown = true
+        if (cell.isMine) {
+            elCell.innerText = MINE
+            gameOver = true
+            gameIsOver()
+            return
+        }
+        else {
+            if (negsCounter === 0) {
+                elCell.innerText = EMPTY
+            }
+            else {
+                elCell.innerText = negsCounter
+            }
+        }
+    }
+}
+
+
+// on right click function
+function onRightClick(elCell, i, j) {
+    checkGameOver(gBoard)
+    var sound = new Audio('js/click.mp3')
+    sound.play()
+    var cell = gBoard[i][j]
+    if (gameOver) return
+    if (!cell.isMarked) {
+        cell.isMarked = true
+        elCell.innerText = FLAG
+
+    }
+    flagConter++
+}
+
+
+// game over function
+function gameIsOver() {
+    const elBtn = document.querySelector('.conteiner button')
+    var sound = new Audio('js/boom.mp3')
+    sound.play()
+    elBtn.innerText = '🥺'
+
+}
+
+
+// check game over function
+function checkGameOver(board) {
+    var allCellsCount = size * size
+    var markedCells = 0
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            var cells = board[i][j]
+            if (cells.isMarked || cells.isShown) markedCells++
+        }
+    }
+    if (allCellsCount === markedCells) victory()
+
+}
+
+
+// victory function
+function victory() {
+    console.log('you won')
+
+}
